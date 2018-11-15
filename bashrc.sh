c50a3e318e3a38827023f1e3cc9c3b9a0c8333bc
Peter Weissig <peterweissig@github.com>
2018-11-15 22:04:12 +0100

    splitted functions.sh into two files (pdf and video)

--- bashrc.sh
+++ bashrc.sh
@@ -7,7 +7,8 @@ temp_local_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd )/"
 
 
 #***************************[source]******************************************
-# 2018 04 01
+# 2018 11 15
 
-. ${temp_local_path}scripts/functions.sh
+. ${temp_local_path}scripts/pdf_functions.sh
+. ${temp_local_path}scripts/video_functions.sh
 . ${temp_local_path}scripts/help.sh
--- scripts/video_functions.sh
+++ scripts/video_functions.sh
@@ -1,43 +1,5 @@
 #!/bin/bash
 
-#***************************[documents]***************************************
-# 2018 08 24
-
-function multimedia_pdf_page_extract() {
-
-    # print help
-    if [ "$1" == "-h" ]; then
-        echo "$FUNCNAME <start> <end> <filename>"
-
-        return
-    fi
-    if [ "$1" == "--help" ]; then
-        echo "$FUNCNAME needs 3 parameters"
-        echo "     #1: number of first page (e.g. 1 == first page)"
-        echo "     #2: number of last page  (e.g. 04)"
-        echo "     #3: document name        (e.g. paper.pdf)"
-        echo "The output file will be named"
-        echo "  \"<filename>_p<start>-p<end>.pdf\"."
-        echo "  (e.g. paper_p1-p04.pdf)"
-
-        return
-    fi
-
-    # check parameter
-    if [ $# -ne 3 ]; then
-        echo "$FUNCNAME: Parameter Error."
-        $FUNCNAME --help
-        return -1
-    fi
-
-    gs -sDEVICE=pdfwrite -dNOPAUSE -dBATCH -dSAFER \
-       -dFirstPage=${1} \
-       -dLastPage=${2} \
-       -sOutputFile=${3%.pdf}_p${1}-p${2}.pdf \
-       ${3}
-}
-
-
 #***************************[video]*******************************************
 # 2018 09 03
 
