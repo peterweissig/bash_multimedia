4d600d476e970c108d13f88200349202ce8a0db1
Peter Weissig <peterweissig@github.com>
2018-11-15 22:19:51 +0100

    added function to extract images from pdf

--- scripts/pdf_functions.sh
+++ scripts/pdf_functions.sh
@@ -37,3 +37,47 @@ function multimedia_pdf_page_extract() {
        ${3}
 }
 
+# 2018 11 15
+
+function multimedia_pdf_images_extract() {
+
+    # print help
+    if [ "$1" == "-h" ]; then
+        echo "$FUNCNAME <filename>"
+
+        return
+    fi
+    if [ "$1" == "--help" ]; then
+        echo "$FUNCNAME needs 1 parameters"
+        echo "     #1: document name        (e.g. paper.pdf)"
+        echo "All images from the document will be stored in the local"
+        echo "  subfolder \"img/\"."
+        echo "  Naming will be <page_nr>-<img_nr>.<extension>"
+        echo "  (e.g. img/001-012.png)"
+
+        return
+    fi
+
+    # check parameter
+    if [ $# -ne 1 ]; then
+        echo "$FUNCNAME: Parameter Error."
+        $FUNCNAME --help
+        return -1
+    fi
+
+    if [ ! -f "$1" ]; then
+        echo "$FUNCNAME: file \"$1\" does not exist."
+        return -1;
+    fi
+
+    if [ ! -d "img/" ]; then
+        echo "mkdir img/"
+        mkdir -p "img/"
+    else
+        echo "folder img/ already exists"
+    fi
+
+    tmp="$(basename "$1")"
+    pdfimages -p -png -j "${1}" "img/${tmp%.*}"
+}
+
