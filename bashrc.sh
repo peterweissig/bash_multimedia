6258dbca494bb39278351dff561db691433aaff7
Peter Weissig <peterweissig@github.com>
2019-12-01 21:15:10 +0100

    updated behaviour of SOURCED_BASH_... variables   (they are now incrementing their values in order)

--- bashrc.sh
+++ bashrc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #***************************[check if already sourced]************************
-# 2018 11 30
+# 2019 12 01
 
 if [ "$SOURCED_BASH_MULTIMEDIA" != "" ]; then
 
@@ -9,7 +9,13 @@ if [ "$SOURCED_BASH_MULTIMEDIA" != "" ]; then
     exit
 fi
 
-export SOURCED_BASH_MULTIMEDIA=1
+if [ "$SOURCED_BASH_LAST" == "" ]; then
+    export SOURCED_BASH_LAST=1
+else
+    export SOURCED_BASH_LAST="$(expr "$SOURCED_BASH_LAST" + 1)"
+fi
+
+export SOURCED_BASH_MULTIMEDIA="$SOURCED_BASH_LAST"
 
 
 #***************************[paths and files]*********************************
